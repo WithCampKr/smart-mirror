fb55202b496b0959c1b7fe0d8885c9a753b859ff
moka-a <aud1220a@gmail.com>
2016-06-09 13:17:52 +0900

    refactor server.js - message for 404 not found

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var path = require( 'path' );
+var path = require('path');
 
 //  set up the template engine
-app.set( 'view engine', 'html' );
-app.set( 'views', path.join( __dirname, '/' ) );
-app.use( express.static( path.join( __dirname, '/' ) ) );
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/'));
+app.use(express.static(path.join(__dirname, '/')));
 
-app.get( '/', function( req, res ) {
-  res.render( 'index.html' );
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.get( '*', function(req, res) {
+  res.writeHead( 404, { "Content-Type": "application/json" } );
+  res.end(JSON.stringify({code: 404, info: "not found"}) + "\n" );
 } );
 
-app.get( '*', fourOhFour );
-function fourOhFour( req, res ) {
-  res.writeHead( 404, { "Content-Type": "application/json" } );
-  res.end( JSON.stringify( new Error( "invalid" ) ) + "\n" );
-}
-
-app.listen( port );
+app.listen(port);
